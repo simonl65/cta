472cf7cac51a6d9f0293a80ad5a7519d8c475642
Simon Lincoln <simon.lincoln@contentdiscovered.com>
2018-12-04 14:31:39 +0000

    feat: new Xmas CTA

---
+++ resources/views/pages/xmas01.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app') 
+@section('css')
+<link rel="stylesheet" href="css/ppichecker/style.css">
+@endsection
+ 
+@section('content')
+<div class="xmas01">
+    <div id="edgemid">
+
+        <div class=" cta-question-buttons">
+            <div class=" cta-question-buttons__title">Have a Relaxing Xmas</div>
+            <div class=" cta-question-buttons__content">
+                {{-- <div class=" cta-question-buttons__fireplace"></div> --}}
+                <p class=" cta-question-buttons__narrative">Let us take the strain of checking all your lenders <span>whilst you relax and enjoy Christmas.</span></p>
+                <a class=" cta-button cta-button--animated" href="http://native.flavourads.com/campaigns/path/out.php">Check Your PPI Now</a>
+                <p class=" cta-question-buttons__disclaimer">Your information will only be used to complete a check for PPI on previous finance you had. All checks are
+                    carried out by Authorised&nbsp;Claims Management Companies.</p>
+            </div>
+        </div>
+
+    </div>
+</div>
+@endsection
